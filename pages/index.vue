1e73d8019a798154fad9286e01a2c8a973ef56b2
Hein Dauven <heindauven@gmail.com>
2019-05-24 01:52:36 +0200

    Add HTML language attribute and add rel attribute to links.

--- pages/index.vue
+++ pages/index.vue
@@ -2,34 +2,34 @@
   <section class="container">
     <div>
       <app-logo/>
-      <h1 class="title">
-        static-vue-netlify-site
-      </h1>
-      <h2 class="subtitle">
-        Static Vue Netlify test website
-      </h2>
+      <h1 class="title">static-vue-netlify-site</h1>
+      <h2 class="subtitle">Static Vue Netlify test website</h2>
       <div class="links">
         <a
           href="https://nuxtjs.org/"
           target="_blank"
-          class="button--green">Documentation</a>
+          rel="noopener"
+          class="button--green"
+        >Documentation</a>
         <a
           href="https://github.com/nuxt/nuxt.js"
           target="_blank"
-          class="button--grey">GitHub</a>
+          rel="noopener"
+          class="button--grey"
+        >GitHub</a>
       </div>
     </div>
   </section>
 </template>
 
 <script>
-import AppLogo from '~/components/AppLogo.vue'
+import AppLogo from "~/components/AppLogo.vue";
 
 export default {
   components: {
     AppLogo
   }
-}
+};
 </script>
 
 <style>
@@ -42,7 +42,8 @@ export default {
 }
 
 .title {
-  font-family: "Quicksand", "Source Sans Pro", -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif; /* 1 */
+  font-family: "Quicksand", "Source Sans Pro", -apple-system, BlinkMacSystemFont,
+    "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif; /* 1 */
   display: block;
   font-weight: 300;
   font-size: 100px;
